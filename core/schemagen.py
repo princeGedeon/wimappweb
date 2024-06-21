d3ecabd00c1b65637143065871429a48b43c908e
princeGedeon <guedjegedeon03@gmail.com>
2024-06-21 09:21:09 +0100

    f

--- core/schemagen.py
+++ core/schemagen.py
@@ -4,5 +4,5 @@ from drf_yasg.generators import OpenAPISchemaGenerator
 class BothHttpAndHttpsSchemaGenerator(OpenAPISchemaGenerator):
     def get_schema(self, request=None, public=False):
         schema = super().get_schema(request, public)
-        schema.schemes = ["https",g]
+        schema.schemes = ["https",]
         return schema
