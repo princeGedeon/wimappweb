4dcde48b6fced837c692f0a15178ba6d16b548e1
princeGedeon <guedjegedeon03@gmail.com>
2024-07-23 20:12:51 +0100

    fix

--- musicapp/urls.py
+++ musicapp/urls.py
@@ -6,7 +6,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework.permissions import AllowAny
 from .views import MusicViewSet, PlaylistViewSet, CreatePlaylistView, GetMyPlaylistsView, \
-    GetMyFavorisView, GetSchoolPlaylistsView, UserFavoritesView, PlaylistMusicsAPIView, GetMyFavoriView, \
+     GetSchoolPlaylistsView, UserFavoritesView, PlaylistMusicsAPIView, GetMyFavoriView, \
     DeleteFavoriView, AddMusicToFavoriView
 
 schema_view = get_schema_view(
@@ -28,7 +28,6 @@ urlpatterns = [
     path('create-playlist/', CreatePlaylistView.as_view(), name='create_playlist'),
     path('get-school-playlists/', GetSchoolPlaylistsView.as_view(), name='get_school_playlists'),
     path('get-my-playlists/', GetMyPlaylistsView.as_view(), name='get_my_playlists'),
-    path('get-my-favoris/', GetMyFavorisView.as_view(), name='get_my_favoris'),
     path('playlists/<int:pk>/musics/', PlaylistMusicsAPIView.as_view(), name='playlist-musics'),
 
     path('my-favorites/', UserFavoritesView.as_view(), name='my-favorites'),
