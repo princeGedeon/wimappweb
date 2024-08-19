678a5f3688c129bcd5be1c668882105112f15e15
princeGedeon <guedjegedeon03@gmail.com>
2024-08-19 09:23:33 +0100

    fix register

--- accountapp/models.py
+++ accountapp/models.py
@@ -11,7 +11,7 @@ class CustomUserManager(BaseUserManager):
         if not email:
             raise ValueError('The Email field must be set')
         email = self.normalize_email(email)
-        user = self.model(email=email,secondary_email="", **extra_fields)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
